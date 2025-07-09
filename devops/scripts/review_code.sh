3c1601e4a52ad753af9524bdfb7fbabf65775146
hark130 <hark130@yahoo.com>
2025-07-09 17:17:02 -0500

    WELL-1: "New" code review script initial commit
    Changes to be committed:
    	new file:   devops/scripts/review_code.sh

---
+++ devops/scripts/review_code.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# DESCRIPTION:
+# This script automates the execution of various checks on the project's code base.  Reviews of
+# the test code are a bit more lenient.  All checks will be run, regardless of what is found, but
+# this script will exit with a non-zero value if any check fails.
+#
+# EXIT CODES:
+# This script uses the following exit codes:
+#   0 on success
+#   1 if any command exits with a non-zero value
+
+# GLOBAL VARIABLES
+EXIT_CODE=0          # Exit code used by this script
+ORIGINAL_DIR=$(pwd)  # Working dir of execution
+
+
+# CHECK IT
+# Pycodestyle
+pycodestyle --max-line-length=100 ./
+if [ $? -eq 0 ]
+then
+    echo -e "\n[✓] Pycodestyle likes the code base\n"
+else
+    echo -e "\n[X] Pycodestyle does *NOT* like the code base\n"
+    EXIT_CODE=1
+fi
+# Pylint (WERE LLAMA)
+find ./well/ -type f -name "*.py" | xargs pylint --score=no
+if [ $? -eq 0 ]
+then
+    echo -e "\n[✓] Pylint approves of WERE LLAMA\n"
+else
+    echo -e "\n[X] Pylint does *NOT* approve of WERE LLAMA\n"
+    EXIT_CODE=1
+fi
+# Pylint (Test Code)
+# find ./test/ -type f -name "*.py" | xargs pylint --score=no --disable=duplicate-code
+# if [ $? -eq 0 ]
+# then
+#     echo -e "\n[✓] Pylint is happy with the test code\n"
+# else
+#     echo -e "\n[X] Pylint is *NOT* happy with the test code\n"
+#     EXIT_CODE=1
+# fi
+
+
+# DONE
+cd $ORIGINAL_DIR
+echo ""
+exit $EXIT_CODE
