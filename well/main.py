024eb271e6de20939c813d3084e3f3c94cc1bab1
hark130 <hark130@yahoo.com>
2025-07-09 00:10:39 -0500

    dev: A huge step for main()
    Basically... it's working.
    The output is good.
    There is some stale DEBUGGING in here but it seems to be working.
    EOD commit.
    
    Changes to be committed:
    	modified:   well/main.py

--- well/main.py
+++ well/main.py
@@ -5,10 +5,10 @@ from collections import OrderedDict
 # Third Party Imports
 # Local Imports
 from well.archive import get_past_answers
-from well.globals import FIVE_LETTER_WORDS
+from well.globals import FIVE_LETTER_WORDS, INPUT_GREEN
 from well.prompt import get_feedback
 from well.word_hints import WordHints
-from well.words import calc_word_ordict, remove_words
+from well.words import calc_word_ordict, remove_word_hint, remove_words
 
 
 def main() -> int:
@@ -34,18 +34,26 @@ def main() -> int:
         # A. Calculate probability of remaining words
         ord_dict = calc_word_ordict(available_list, unique=unique)
         unique = False
-        print(f'TOP TEN: {", ".join(list(ord_dict.keys())[:10])}')
-        # B. Take feedback
-        (temp_word, temp_result) = get_feedback()
-        word_hint.update_word(temp_word, temp_result)
-        # C. Remove invalid words
+        print(f'TOP GUESSES ({len(ord_dict)} remaining): {", ".join(list(ord_dict.keys())[:10])}')
+        try:
+            # B. Take feedback
+            (temp_word, temp_result) = get_feedback()
+            if temp_result == (INPUT_GREEN * 5):
+                print('Congratulations!')
+                break  # All done
+            word_hint.update_word(temp_word, temp_result)
+            # C. Remove invalid words
+            available_list = remove_word_hint(available_list, word_hint)
+        except (TypeError, ValueError) as err:
+            print(f'Bad input encountered: {repr(err)}')
+            print('Try again.\n')
 
-        print(f'FIRST:  {word_hint.first.excluded}')  # DEBUGGING
-        print(f'SECOND: {word_hint.second.excluded}')  # DEBUGGING
-        print(f'THIRD:  {word_hint.third.excluded}')  # DEBUGGING
-        print(f'FOURTH: {word_hint.fourth.excluded}')  # DEBUGGING
-        print(f'FIFTH:  {word_hint.fifth.excluded}')  # DEBUGGING
-        break  # DEBUGGING
+        # print(f'FIRST:  {word_hint.first.excluded}')  # DEBUGGING
+        # print(f'SECOND: {word_hint.second.excluded}')  # DEBUGGING
+        # print(f'THIRD:  {word_hint.third.excluded}')  # DEBUGGING
+        # print(f'FOURTH: {word_hint.fourth.excluded}')  # DEBUGGING
+        # print(f'FIFTH:  {word_hint.fifth.excluded}')  # DEBUGGING
+        # break  # DEBUGGING
 
     # DONE
     return result
