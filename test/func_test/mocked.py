2d473eeeaa30fbf4d499ee2dfc27ad1fe8a44e1c
hark130 <hark130@yahoo.com>
2025-07-11 19:31:53 -0500

    WELL-2: Big refactor of get_mocked_feedback()
    The results are now mutable (which is key).
    Function makes two passes: greens, then yellows.
    Default status is skip.
    All test cases passing.
    
    Time to add some more legacy tests that further reflect reality.
    
    Changes to be committed:
    	modified:   test/func_test/mocked.py
    
    Changes not staged for commit:
    	modified:   well/main.py  # Shunted around the archive pull

--- test/func_test/mocked.py
+++ test/func_test/mocked.py
@@ -21,8 +21,8 @@ def get_mocked_feedback(guess: str, wordle: str) -> str:
             well.globals.INPUT_SKIP
     """
     # LOCAL VARIABLES
-    result = ''   # Mocked feedback
     matches = ''  # Store letters already been accounted for
+    result = [INPUT_SKIP, INPUT_SKIP, INPUT_SKIP, INPUT_SKIP, INPUT_SKIP]   # Mocked feedback
 
     # INPUT VALIDATION
     # guess
@@ -35,28 +35,21 @@ def get_mocked_feedback(guess: str, wordle: str) -> str:
         raise ValueError(f'The wordle value "{wordle}" is not five characters long!')
 
     # GET IT
+    # Find the direct hits
     for index in range(5):
-    # for g_char, w_char in zip(guess, wordle):
         g_char = guess[index]
         w_char = wordle[index]
         if g_char == w_char:
-            result = result + INPUT_GREEN
-            matches = matches + g_char  # Accounted for
-        elif g_char in wordle:
-            if 1 == wordle.count(g_char):
-                result = result + INPUT_YELLOW
-                matches = matches + g_char  # Accounted for
-            else:
-                # Multiple occurences of g_char in the wordle
-                print(f'\nGUESS:   {guess}')  # DEBUGGING
-                print(f'WORDLE:  {wordle}')  # DEBUGGING
-                print(f'MATCHES: {matches}')  # DEBUGGING
-                if wordle.count(g_char) == matches.count(g_char):
-                    result = result + INPUT_SKIP  # All matches for this letter accounted for
-                else:
-                    result = result + INPUT_YELLOW
-        else:
-            result = result + INPUT_SKIP
+            result[index] = INPUT_GREEN
+            matches = matches + g_char
+    # Find the rest
+    for index in range(5):
+        g_char = guess[index]
+        w_char = wordle[index]
+        if g_char in wordle and INPUT_SKIP == result[index]:
+            if wordle.count(g_char) > matches.count(g_char):
+                result[index] = INPUT_YELLOW
+                matches = matches + g_char
 
     # DONE
-    return result
+    return ''.join(result)
