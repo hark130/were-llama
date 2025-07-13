a842ca90ebbe627a41f5913f5df30f8f2d46c97d
hark130 <hark130@yahoo.com>
2025-07-12 19:45:01 -0500

    WELL-7: Updated __main__ to exit with main()
    Changes to be committed:
    	modified:   well/__main__.py
    
    Changes not staged for commit:
    	modified:   well/main.py

--- well/__main__.py
+++ well/__main__.py
@@ -1,7 +1,11 @@
 """Entry point for the WERE LLAMA (WALL) Python package."""
 
+# Standard Imports
+import sys
+# Third Party Imports
+# Local Imports
 from well.main import main
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
