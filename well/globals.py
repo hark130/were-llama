af14c7555fefe1c6bb91d58f1c11c78a9a3d521a
hark130 <hark130@yahoo.com>
2025-07-21 19:41:29 -0500

    WELL-4: Updated default starting weight in globals
    Functional testing highlighted that, for the linear implementation of
    determine_dupe_weight(), as starting weight of 45.5% is optimal.
    
    45% and 46% actually tied with 45.5% (I thought it might spike) but a
    special DEBUGGING statement in the test framework helped verify they all
    had the same "average solve" rate.
    
    0.45  AVG SOLVED: 93.88570435990967
    0.455 AVG SOLVED: 93.88570435990967
    0.46  AVG SOLVED: 93.88570435990967
    
    Changes to be committed:
    	modified:   well/globals.py
    
    Changes not staged for commit:
    	modified:   test/func_test/test_strategies.py

--- well/globals.py
+++ well/globals.py
@@ -21,7 +21,7 @@ INPUT_SKIP: Final[str] = ' '.lower()
 INPUT_SKIP_TITLE: Final[str] = '<SPACE>'.upper()
 
 # STRATEGY MACROS
-DEF_START_WEIGHT: Final[float] = .75  # Default starting weight for words w/ duplicate letters
+DEF_START_WEIGHT: Final[float] = 0.455  # Default starting weight for words w/ duplicate letters
 
 # WORD MACROS
 # Relative frequencies of the first letters of a word in English language
